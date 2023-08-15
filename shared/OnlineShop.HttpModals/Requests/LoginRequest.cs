fa22627f77e4ee88091236e2d0829d1bff61e860
ksenia <ksy184694@gmail.com>
2023-08-15 13:48:32 +0300

    changes in LoginRequest

--- shared/OnlineShop.HttpModals/Requests/LoginRequest.cs
+++ shared/OnlineShop.HttpModals/Requests/LoginRequest.cs
@@ -6,10 +6,10 @@ namespace OnlineShop.HttpModals.Requests
     public class LoginRequest
     {
         [Required, EmailAddress]
-        public string Email { get; }
+        public string Email { get; set; }
 
         [Required]
         [StringLength(30, ErrorMessage = "Пароль минимум 8 символов", MinimumLength = 8)]
-        public string Password { get; }
+        public string Password { get; set; }
     }
 }
